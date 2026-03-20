24a53f0c9e3ce07dd0823f8585b6228e10795039
solanoe <solanoepalacio@gmail.com>
2026-03-20 10:12:30 -0300

    adds missing AuthModule to household controller

--- apps/api/src/household/household.module.ts
+++ apps/api/src/household/household.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { HouseholdController } from './household.controller';
 import { HouseholdService } from './household.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [HouseholdController],
   providers: [HouseholdService],
 })
